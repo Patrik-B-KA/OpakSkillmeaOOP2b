d8d6be01de37af2fa976b037133d3963a0a15b5d
Patrik-B-KA <patrik.botlo@gmail.com>
2025-12-27 12:33:58 +0100

    trieda Banka

---
+++ src/Banka.java
@@ -0,0 +1,16 @@
+public class Banka {
+    private int roky;
+    private float pociatocnyVklad;
+    private float urokovaSadzba;
+    private float poplatky;
+    private float konecnyZostatok;
+
+    Banka(int roky, float pociatocnyVklad, float urokovaSadzba, float poplatky){
+        this.roky = roky;
+        this.pociatocnyVklad = pociatocnyVklad;
+        this.urokovaSadzba = urokovaSadzba;
+        this.poplatky = poplatky;
+
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -32,11 +32,11 @@ public class Main {
         System.out.println(cenaKauflandu);*/
 
 
-        Pes dunco = new Pes("CV", 45.5f,true, 12);
+        /*Pes dunco = new Pes("CV", 45.5f,true, 12);
         dunco.vydajZvuk();
 
         Macka muro = new Macka("siamska", 7.7f, true, 13);
-        muro.vydajZvuk();
+        muro.vydajZvuk();*/
 
 
 
